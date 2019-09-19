03d91b4ba8383345acf033a4c000a3b5e6092550
Christian H. Bohlbro <cboh@danskebank.dk>
2019-09-19 09:23:17 +0200

    Refactoring

--- packages/pie-chart/src/Charts/Pie.js
+++ packages/pie-chart/src/Charts/Pie.js
@@ -14,25 +14,36 @@ const PieChart = ({
 }) => {
   const total = values.reduce((a, b) => a + b, 0);
 
-  let cumulativePercent = 0;
-
-  const paths = values.map((val) => {
-    const percent = val / total;
-    console.log(total, percent);
+  /**
+   * Return x and y coordinates in the set [0, 1] based on the percentage value.
+   * Assumes the percentage is given in the set [0, 1].
+   *
+   * @param {Number}  percent The percentage
+   * @returns {Array}         An array with the x and y values.
+   */
+  const getCoordsFromPercent = (percent) => {
     const x = Math.cos(2 * Math.PI * percent);
     const y = Math.sin(2 * Math.PI * percent);
-    const rotation = 360 * cumulativePercent;
-    const flag = percent > 0.5 ? 1 : 0;
 
-    cumulativePercent += percent;
+    return [x, y];
+  };
 
-    return {
-      x,
-      y,
-      rotation,
-      flag,
-    };
-  });
+  /**
+   * Returns a rotation based on the previous percentages, for aligning all the
+   * chart cuts. Assumes the percentage is given in the set [0, 1].
+   *
+   * @param {Number} percent The percentage
+   */
+  const getCumulativeRotation = (percent) => {
+    if (typeof getCumulativeRotation.val === 'undefined') {
+      getCumulativeRotation.val = 0;
+    }
+
+    const rotation = 360 * getCumulativeRotation.val;
+    getCumulativeRotation.val += percent;
+
+    return rotation;
+  };
 
   return (
     <div className="pie-chart">
@@ -43,24 +54,28 @@ const PieChart = ({
         style={{ transform: 'rotate(-0.25turn)' }}
       >
         {debug && (<circle cx="0" cy="0" r="1" fill="white" />)}
-        {paths.map(({
-          x,
-          y,
-          rotation,
-          flag,
-        }) => (
-          <path
-            key={`${x}-${y}-${rotation}`}
-            d={`
+        {values.map((val) => {
+          const percent = val / total;
+
+          const [x, y] = getCoordsFromPercent(percent);
+          const rotation = getCumulativeRotation(percent);
+
+          const flag = percent > 0.5 ? 1 : 0;
+
+          return (
+            <path
+              key={`${x}-${y}-${rotation}`}
+              d={`
               M 1 0
               A 1 1 0 ${flag} 1 ${x} ${y}
               L 0 0
               Z
             `}
-            transform={`rotate(${rotation})`}
-            fill={colorFactory.next().value}
-          />
-        ))}
+              transform={`rotate(${rotation})`}
+              fill={colorFactory.next().value}
+            />
+          );
+        })}
       </svg>
     </div>
   );
